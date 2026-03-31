1d87c5b4c080353a4ffb8e0fd59e8926401cdead
benjaminquepasa <alexandermaldonado321.0@gmail.com>
2026-03-31 11:35:20 -0300

    avance

--- static/js/script.js
+++ static/js/script.js
@@ -3,21 +3,39 @@ function registroPagos() {
     const input = document.getElementById("input1");
     const result = document.getElementById("resultado1")
     const container = document.getElementById("container1");
-    let monto = parseInt(input.value);
-    let descuento1 = 70000 * 15 / 100;
-    let descuento2 = 70000 * 5 / 100;
-    let total = pagoPermiso(monto)
-    result.textContent = total;
+    let monto = parseFloat(input.value);
+    let descuento = pagoPermiso(monto)
+    result.textContent = descuento;
     input.value = "";
     container.classList.remove("d-none");
 };
 
 function pagoPermiso(monto) {
-    if (monto > 70000) {
-        return descuento1;
-    } else if (monto < 70000) {
-        return descuento2;
+    if (monto >= 70000) {
+        return `El total con descuento es de $ ${monto * 15 / 100}`
     } else {
-        return `El total a pagar es de ${total}`
+        return `El total con descuento es de $ ${monto * 5 / 100}`
     }
+};
+
+//Ejercicio n°2
+
+let fila = [];
+function agregarLista(nombre) {
+    fila.push(nombre);
+    return fila.join(", ");
+    fila.unshift(urgencia);
+    return fila.join(", ");
+};
+
+function listaVacunacion() {
+    const container = document.getElementById("container1");
+    const result = document.getElementById("resultado1");
+    const input = document.getElementById("input1");
+    let nombre = input.value;
+    let urgencia = a;
+    let resultado = agregarLista(nombre);
+    result.textContent = resultado;
+    input.value = "";
+    container.classList.remove("d-none");
 };
